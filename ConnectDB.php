30138ead223957ac47c82e6111fd0eb994f79c0a
dobrodeev <dobrod@meta.ua>
2018-05-24 19:18:06 +0300

    создали файл для подключения к БД

---
+++ ConnectDB.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'ConnectDB.php';
+$login = 'root';
+$password = '';
+$localhost = 'localhost';
+$db = 'regandauto';
+$connect = mysqli_connect($localhost, $login, $password, $db);
+if (!$connect)
+{
+    die('Error');
+}

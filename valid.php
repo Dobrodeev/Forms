c2622c7d87ab5ef92fac4bf8c03d037049a2ff36
dobrodeev <dobrod@meta.ua>
2018-05-24 19:17:24 +0300

    создали подключение к БД для сохранения логинов и паролей, а также сделали проверки перед записью в БД

--- valid.php
+++ valid.php
@@ -11,14 +11,22 @@ if (isset($_POST['go']))
 {
     if ($_POST['username'] && $_POST['userlogin'] && $_POST['userpass'] && $_POST['usermail'])
     {
-        $userName = clear($_POST['username']);
-        $userLogin = clear($_POST['userlogin']);
+        $userName = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['username'])));
+        $userLogin = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['userlogin'])));
         $userPass = md5(clear($_POST['userpass'])); //очищаем и хешируем пароль
-        $userMail = clear($_POST['usermail']);
+        $userMail = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['usermail'])));
         if (!preg_match("|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is", $userMail))
         {
             $error[0] = 'style="background-color: #cc4c33";';
 //            "|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is"
+        }
+        $query = mysqli_query($connect, "SELECT * FROM reg WHERE  usermail = '$userMail'");
+        $num = mysqli_num_rows($query);
+        if ($num == 0 && ($error[0]) == '')
+        {
+            $insert = mysqli_query($connect, "INSERT INTO reg (username, userlogin, userpass, usermail) VALUES ('{$userName}', '{$userLogin}'),
+            '{$userPass}', '{$userMail}'");
+
         }
     }
 }

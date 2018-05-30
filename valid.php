7f6118e01beef9c822dbce90c0a6926d41cc0bae
dobrodeev <dobrod@meta.ua>
2018-05-30 18:37:40 +0300

    исправили ошибку: с помощью clear( чистим только $_POST[]), добавили проверки на корректность мыла

--- valid.php
+++ valid.php
@@ -13,25 +13,38 @@ if (isset($_POST['go']))
 {
     if ($_POST['username'] && $_POST['userlogin'] && $_POST['userpass'] && $_POST['usermail'])
     {
-        $userName = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['username'])));
-        $userLogin = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['userlogin'])));
+
+        $userName = htmlentities(mysqli_real_escape_string($connect, clear($_POST['username'])));
+        $userLogin = htmlentities(mysqli_real_escape_string($connect, clear($_POST['userlogin'])));
         $userPass = md5(clear($_POST['userpass'])); //очищаем и хешируем пароль
-        $userMail = htmlentities(mysqli_real_escape_string(clear($connect, $_POST['usermail'])));
+        $userMail = htmlentities(mysqli_real_escape_string($connect, clear($_POST['usermail'])));
         if (!preg_match("|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is", $userMail))
         {
             $error[0] = 'style="background-color: #cc4c33";';
-//            "|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is"
         }
-//        $query = mysqli_query($connect, "SELECT * FROM reg WHERE usermail = '$userMail'");
-//        $num = mysqli_num_rows($query);
-        $query2 = "INSERT INTO reg (username, userlogin, userpass, usermail) VALUES ('{$userName}', '{$userLogin}', '{$userPass}', '{$userMail}'";
-//        $query3 = "INSERT INTO reg (username, userlogin, userpass, usermail) VALUES ('Yriy', 'Boyco', 'boyco1960', 'boyco@meta.ua'";
-        $insert = mysqli_query($connect, $query2);
-//        $insert = mysqli_query($connect, $query3);
-//        if ($num == 0 && ($error[0]) == '')
-//        {
-//            $insert = mysqli_query($connect, $query2);
-//        }
+
+        $query = mysqli_query($connect, "SELECT * FROM reg WHERE usermail = '$userMail'");
+        $num = mysqli_num_rows($query);
+        if ($num == 0 && ($error[0]) == '')
+        {
+//            echo 1;
+            $query2 = "INSERT INTO reg (username, userlogin, userpass, usermail) VALUES ('{$userName}', '{$userLogin}', '{$userPass}', '{$userMail}')";
+            $insert = mysqli_query($connect, $query2);
+            $result = 'Все успешно';
+            $login = '<a href="auto.php"> перейти на авторизацию </a>';
+        }
+        elseif ($error != '') {
+            $result = 'Введите корректное мыло';
+        }
+            else
+            {
+                $result = 'Мыло уже существует';
+            }
+        }
+        else
+        {
+            $result = 'Заполните пустые строки';
+        }
 //        if($insert)
 //            {
 //                echo "<span style='color:blue;'>Данные добавлены</span>";
@@ -40,5 +53,5 @@ if (isset($_POST['go']))
 //            echo "<span style='color:blue;'>Данные не добавлены</span>";
 //        // закрываем подключение
 //        mysqli_close($insert);
-    }
+
 }
